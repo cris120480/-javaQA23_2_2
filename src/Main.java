1c3244f8f0e73e353a4cfb598929ef7feb3b3d80
Денис Гришкевич <cris12@mail.ru>
2021-09-20 21:52:35 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        float balanceNow = 100.00F;
+        float transactionNew = 1955.00F;
+
+        if (transactionNew > 1000) {
+           transactionNew =  transactionNew + (int) transactionNew / 100;
+
+    } else {
+            transactionNew =  transactionNew;
+    }
+        float totalBalance = balanceNow +  transactionNew;
+           System.out.println(totalBalance);
+    }
+    }
+
+
+
